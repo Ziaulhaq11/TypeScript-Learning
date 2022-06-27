3c8ddbc39105fe48ff2ea3c9203ab2b1590abf23
ziaullhaq11 <ziaullhaq11@gmail.com>
2022-06-27 08:54:34 +0530

    other drag&drop folder is deleted

--- 7DragAndDrop/src/app.ts
+++ 7DragAndDrop/src/app.ts
@@ -1,8 +1,22 @@
-//Project State management
+enum ProjectStatus {Active, Finished}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people : number, 
+    public status : ProjectStatus
+  ) {}
+}
+
+//Project State Management
+
+type Listener = (items: Project[]) => void;
 
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
   
   private constructor() {
@@ -17,19 +31,15 @@ class ProjectState {
     return this.instance
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn)
   }
 
   addProject(title : string, description : string, numOfPeople: number) {
-    const newProject = {
-      id: Date.now().toString(),
-      title: title,
-      description: description,
-      people : numOfPeople
-    }
+    const newProject = new Project(Math.random.toString(), title, description, numOfPeople,ProjectStatus.Active )
     this.projects.push(newProject)
     for (const listenerFn of this.listeners) {
+      console.log(listenerFn)
       listenerFn(this.projects.slice())
     }
   }
@@ -94,7 +104,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects : any[]
+  assignedProjects : Project[]
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = <HTMLTemplateElement>(
@@ -109,8 +119,14 @@ class ProjectList {
     );
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`
-    projectState.addListener((projects:any[]) => {
-      this.assignedProjects = projects;
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active
+        }
+        return prj.status === ProjectStatus.Finished
+      })
+      this.assignedProjects = relevantProjects;
       this.renderProjects()
     })
     this.attach()
@@ -119,6 +135,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`) as HTMLUListElement;
+    listEl.innerHTML = '' //because checking entries in DOM which has rendered will be costly so emptying all of that and then rendering via that. To avoid duplication
     //it will contain all the lists
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li')
